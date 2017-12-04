ff1c68b40c0022d51088288d49b35ff157a386a3
jpolvora <jpolvora@gmail.com>
2017-12-04 11:24:21 -0300

    await support for actions

--- demo/controllers/home.js
+++ demo/controllers/home.js
@@ -1,19 +1,34 @@
-module.exports = function ({ view }) {
-    return {
-        catchAll: (req) => {
-            /* returning a function that accepts res parameter (the response object from express) */
-            return function (res) {
-                res.send("When an action is not found, the catchAll enters in action.");
-            }
-        },
+function dataService() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('ok')
+        }, 500);
+    });
+}
 
-        index: (req) => {
-            return view("index", { title: "express..." })
-        },
+module.exports = class {
+    constructor(services) {
+        this.view = services.view;
+    }
 
-        about: (req) => {
-            /* returning a string that will be rendered by res.send */
-            return "I'm about page."
+    catchAll(req) {
+        /* returning a function that accepts res parameter (the response object from express) */
+        return function (res) {
+            res.send("When an action is not found, the catchAll enters in action.");
         }
+    }
+
+    index(req) {
+        return this.view("index", { title: "express..." })
+    }
+
+    about(req) {
+        /* returning a string that will be rendered by res.send */
+        return "I'm about page."
+    }
+
+    async promise(req) {
+        await dataService()
+        return "promise success!"
     }
 }
